80d83b6822065b7316bc3614bb6654c01d99df26
Fabian Beltran <fabianbeltran1224@gmail.com>
2022-07-22 01:05:26 -0500

    Part 1, exercise 8 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -17,21 +17,35 @@ const Panel = ({ text, value }) => {
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
+  
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   const addOne = (value,setValue) => setValue(value + 1)
 
+  let all = bad + neutral + good
+
+  let average = (good - bad)/(all)
+
   return (
     <div>
+
       <h1>Give feedback</h1>
+      
       <Button handleClick={() => addOne(good, setGood)}  text={"good"}></Button>
       <Button handleClick={() => addOne(neutral, setNeutral)} text={"neutral"}></Button>
       <Button handleClick={() => addOne(bad, setBad)} text={"bad"}></Button>
+      
       <h1>statistics</h1>
+      
       <Panel text={"good"} value={good}></Panel>
       <Panel text={"neutral"} value={neutral}></Panel>
       <Panel text={"bad"} value={bad}></Panel>
+
+      <Panel text={"all"} value={all}></Panel>
+      <Panel text={"average"} value={average}></Panel>
+      <Panel text={"positivie"} value={(good/(all)*100)}></Panel>
+
     </div>
   )
 }
